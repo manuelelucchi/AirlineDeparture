782b957ed2b7e4d1fc748214b9417080c3c2fd9c
Davide Tricella <dadi.tricella@gmail.com>
2022-07-12 23:59:21 +0200

    Data conversion to DataFrame added

--- src/download.py
+++ src/download.py
@@ -7,8 +7,7 @@ os.environ['KAGGLE_KEY'] = "5d0e079e57605185aeda37716bca4471"
 dataset = 'yuanyuwendymu/airline-delay-and-cancellation-data-2009-2018'
 path = './data'
  
-api = KaggleApi()
-api.authenticate()
-api.dataset_download_files(dataset, path)
-
-#kaggle datasets download -d yuanyuwendymu/airline-delay-and-cancellation-data-2009-2018
+def DownloadData():
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files(dataset, path, unzip=True)
---
+++ src/read.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+path= './data'
+files:list = os.listdir('./data')
+
+def GetAllFrames():
+    big_frame:pd.DataFrame = pd.DataFrame()
+
+    for f in files:
+        if f.endswith('.csv'):
+            frame:pd.DataFrame = pd.read_csv(path + '/' + f)
+            big_frame = pd.concat([big_frame, frame])
+
+            print(big_frame)
+            print('frame ' + f + ' done')
+    return big_frame
+
+def GetFirstFrame():
+    return pd.read_csv(path + '/' + files[0])
