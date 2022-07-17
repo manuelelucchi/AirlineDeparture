f1966483e3b9314932ad74e4ca8929e60a9e65ee
Manuele Lucchi <m.lucchi@egeatech.com>
2022-07-17 13:56:53 +0200

    improvements

--- src/math.py
+++ src/math.py
@@ -1,9 +1,48 @@
 import numpy as np
 
 
-def sigmoid(data):
+def sigmoid(x: float) -> float:
     '''
     Calculates the sigmoid of the given data
     '''
-    g = 1.0 / (1.0 + np.exp(-data))
+    g = 1.0 / (1.0 + np.exp(-x))
     return g
+
+
+def binary_cross_entropy(y: float, y_hat: float) -> float:
+    '''
+    Calculates the binary cross entropy loss of the calculated y and the given y_hat
+    '''
+    loss = -np.mean(y*(np.log(y_hat)) - (1-y)*np.log(1-y_hat))
+    return loss
+
+
+def normalize(X: np.matrix) -> np.matrix:
+    '''
+    Normalizes the data based on the number of features
+    '''
+    # _ -> number of training examples
+    # n -> number of features
+    _, n = X.shape
+
+    for _ in range(n):
+        X = (X - X.mean(axis=0))/X.std(axis=0)
+
+    return X
+
+
+def gradients(X, y, y_hat):
+    '''
+    Calculates the gradient w.r.t weights and bias
+    '''
+
+    # m-> number of training examples.
+    m = X.shape[0]
+
+    # Gradient of loss w.r.t weights.
+    dw = (1/m)*np.dot(X.T, (y_hat - y))
+
+    # Gradient of loss w.r.t bias.
+    db = (1/m)*np.sum((y_hat - y))
+
+    return dw, db
--- src/preprocess.py
+++ src/preprocess.py
@@ -2,8 +2,8 @@ import sys
 from pandas import DataFrame
 
 default_values: dict = {
-    'CANCELLED' : 0,
-    'DIVERTED' : 0
+    'CANCELLED': 0,
+    'DIVERTED': 0
 }
 
 columns_to_remove_for_canceled: list[str] = [
@@ -25,7 +25,7 @@ columns_to_remove_for_canceled: list[str] = [
 
 
 def preprocess_for_canceled(data: DataFrame) -> DataFrame:
-    
+
     # Remove useless columns
     data.drop(columns_to_remove_for_canceled, axis=1, inplace=True)
 
@@ -34,23 +34,31 @@ def preprocess_for_canceled(data: DataFrame) -> DataFrame:
 
 def preprocess(data: DataFrame) -> DataFrame:
 
+    data = common_preprocess(data)
     if sys.argv[1] == "canceled":
         return preprocess_for_canceled(data)
     else:
         raise NotImplementedError()
+
 
 def common_preprocess(data: DataFrame) -> DataFrame:
 
     # Remove the empty columns
     data.dropna(how='all', axis='columns', inplace=True)
 
-    #Replace Nan values with the correct default values
+    # Replace Nan values with the correct default values
     data.fillna(value=default_values, axis='columns', inplace=True)
 
-    #Remove rows with Nan key values
+    # Remove rows with Nan key values
     data.dropna(how='any', axis='index', inplace=True)
 
+    return data
 
-#Division between training set and testing set
 
-#Chiedere cosa fare in caso di valori null su colonne possibilmente rilevanti
+# Division between training set and testing set
+
+# Chiedere cosa fare in caso di valori null su colonne possibilmente rilevanti
+
+# Trasformare tutto in numeri
+
+# Normalizzare i dati tra -1 e 1 (?)
