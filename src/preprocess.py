c4d63b083230969cdeb8901f279a900d79130061
Davide Tricella <56402624+dadit97@users.noreply.github.com>
2022-07-18 10:50:15 +0200

    Csv reading and pre-processing improved

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,20 +7,11 @@ default_values: dict = {
 }
 
 columns_to_remove_for_canceled: list[str] = [
-    'WHEELS_OFF',  # moment when the aircraft takes off
-    'WHEELS_ON',  # moment when the aircraft land
-    # time required to taxi from the landing strip (wheels_on) to the terminal
-    'TAXI_IN',
-    # time required to taxi from the terminal to the landing strip(wheels off)
-    'TAXI_OUT',
-    'OP_CARRIER_FL_NUM',  # flight number assigned from the carrier
-    'DEP_TIME',  # actual time of departure
-    'DEP_DELAY',  # departure delay
-    'ARR_TIME',  # actual time of arrival
-    'ARR_DELAY',  # delay on arrival
     'DIVERTED',  # the flight has been diverted to an unplanned airport
-    'ACTUAL_ELAPSED_TIME',  # time from taxi_in to taxi_out
-    'AIR_TIME'  # time between wheels_off and wheels_on
+]
+
+columns_to_remove_for_diverted: list[str] = [
+    'CANCELLED',  # the flight has been cancelled
 ]
 
 
@@ -31,23 +22,27 @@ def preprocess_for_canceled(data: DataFrame) -> DataFrame:
 
     return data
 
+def preprocess_for_diverted(data: DataFrame) -> DataFrame:
+
+    # Remove useless columns
+    data.drop(columns_to_remove_for_diverted, axis=1, inplace=True)
+
+    return data
+
 
 def preprocess(data: DataFrame) -> DataFrame:
 
     data = common_preprocess(data)
     if sys.argv[1] == "canceled":
         return preprocess_for_canceled(data)
-    else:
-        raise NotImplementedError()
+    if sys.argv[1] == "diverted":
+        return preprocess_for_diverted(data)
 
 
 def common_preprocess(data: DataFrame) -> DataFrame:
 
-    # Remove the empty columns
-    data.dropna(how='all', axis='columns', inplace=True)
-
     # Replace Nan values with the correct default values
-    data.fillna(value=default_values, axis='columns', inplace=True)
+    data.fillna(default_values, inplace=True)
 
     # Remove rows with Nan key values
     data.dropna(how='any', axis='index', inplace=True)
@@ -58,6 +53,7 @@ def common_preprocess(data: DataFrame) -> DataFrame:
 # Division between training set and testing set
 
 # Chiedere cosa fare in caso di valori null su colonne possibilmente rilevanti
+# Chiedere se i dati su delay causati da cose come aereo in ritardo o meteo sono disponibili al momento del calcolo
 
 # Trasformare tutto in numeri
 
--- src/read.py
+++ src/read.py
@@ -3,6 +3,18 @@ import pandas as pd
 from pandas import DataFrame
 
 path = './data'
+columns_to_get: list[str] = [
+    'FL_DATE',
+    'OP_CARRIER',
+    'ORIGIN',
+    'DEST',
+    'CRS_DEP_TIME',
+    'CRS_ARR_TIME',
+    'CANCELLED',
+    'DIVERTED',
+    'CRS_ELAPSED_TIME',
+    'DISTANCE'
+]
 
 
 def get_all_frames() -> DataFrame:
@@ -11,7 +23,8 @@ def get_all_frames() -> DataFrame:
 
     for f in files:
         if f.endswith('.csv'):
-            frame = pd.read_csv(path + '/' + f)
+            # Reading only data at disposal before departure
+            frame = pd.read_csv(filepath_or_buffer=path + '/' + f, usecols=columns_to_get)
             big_frame = pd.concat([big_frame, frame])
             print('Frame ' + f + ' loaded')
     return big_frame
@@ -19,4 +32,6 @@ def get_all_frames() -> DataFrame:
 
 def get_first_frame() -> DataFrame:
     files: list = os.listdir('./data')
-    return pd.read_csv(path + '/' + files[0])
+    big_frame = pd.read_csv(filepath_or_buffer=path + '/' + files[0], usecols=columns_to_get)
+    print('First frame loaded')
+    return big_frame
