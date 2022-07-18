5d79900c91425d103b5c8eef19a5f6a529dadcfd
Davide Tricella <56402624+dadit97@users.noreply.github.com>
2022-07-18 17:06:42 +0200

    Dates and names conversion to numeric values added

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,4 +1,7 @@
+import datetime as dt
 import sys
+from numpy import ndarray
+import numpy
 from pandas import DataFrame
 
 default_values: dict = {
@@ -12,6 +15,22 @@ columns_to_remove_for_canceled: list[str] = [
 
 columns_to_remove_for_diverted: list[str] = [
     'CANCELLED',  # the flight has been cancelled
+]
+
+names_columns_to_convert: list[str] = [
+    'OP_CARRIER',
+    'ORIGIN',
+    'DEST',
+]
+
+date_columns_to_convert: list[str] = [
+    'FL_DATE'
+]
+
+time_columns_to_convert: list[str] = [
+    'CRS_DEP_TIME',
+    'CRS_ARR_TIME',
+    'CRS_ELAPSED_TIME'
 ]
 
 
@@ -47,7 +66,54 @@ def common_preprocess(data: DataFrame) -> DataFrame:
     # Remove rows with Nan key values
     data.dropna(how='any', axis='index', inplace=True)
 
+    convert_names_into_numbers(data)
+    convert_dates_into_numbers(data)
+    convert_times_into_numbers(data)
+
     return data
+
+def convert_names_into_numbers(data: DataFrame) -> DataFrame:
+
+    for c in names_columns_to_convert:
+        unique_values:ndarray = []
+        values_map:dict = {}
+        counter:float = 0
+
+        unique_values = data[c].unique()
+        unique_values = numpy.sort(unique_values)
+        adder:float = 1 / len(unique_values)
+
+        for v in unique_values:
+            values_map[v] = counter
+            counter += adder
+        
+        data[c].replace(to_replace=values_map, inplace=True)
+    
+    return data
+
+def convert_dates_into_numbers(data: DataFrame) -> DataFrame:
+
+    multiplier:float = 1 / 365
+
+    for c in date_columns_to_convert:
+        unique_values:ndarray = []
+        values_map:dict = {}
+
+        unique_values = data[c].unique()
+        unique_values = numpy.sort(unique_values)
+        
+        for v in unique_values:
+            date = dt.datetime.strptime(v, "%Y-%m-%d")
+            day = date.timetuple().tm_yday
+            values_map[v] = day * multiplier
+        
+        data[c].replace(to_replace=values_map, inplace=True)
+
+    return data
+
+def convert_times_into_numbers(data: DataFrame) -> DataFrame:
+
+    raise NotImplementedError
 
 
 # Division between training set and testing set
