cacf3d24e3a9e15a2cbc4ed058deee66d2fa70b7
Davide Tricella <56402624+dadit97@users.noreply.github.com>
2022-08-27 15:19:53 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import sys
-from turtle import forward
-
-from numpy import positive
+from functions import binary_cross_entropy
 from model import Model
 from preprocess import preprocess
+from preprocess import preprocess
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
 
 train_data, train_labels, test_data, test_labels = preprocess()
 
@@ -12,28 +12,20 @@ train_data, train_labels, test_data, test_labels = preprocess()
 #    train_data.shape, train_labels.shape, test_data.shape, len(test_labels)))
 
 if sys.argv[2] == "custom":
-    model = Model(batch_size=train_data.size, learning_rate=0.01)
+    model = Model(batch_size=train_data.size, learning_rate=1)
     model.train(train_data,
-                train_labels, iterations=100000)
+                train_labels, iterations=100)
 else:
     model = LogisticRegression()
     model.fit(train_data, train_labels)
 
-predictions = []
-
-for test_sample, test_label in zip(test_data, test_labels):
-    if sys.argv[2] == "custom":
-        res = model.forward(test_sample)
-    else:
-        res = model.predict(test_sample.reshape([1, test_sample.size]))[0]
-    predictions.append([res, test_label])
-
-correct = len(list(filter(lambda x: round(x[0]) == x[1], predictions)))
-canceled = len(
-    list(filter(lambda x: round(x[0]) == 1 and x[1] == 1, predictions)))
+if sys.argv[2] == "custom":
+    res = model.forward(test_data)
+    loss = binary_cross_entropy(res, test_labels)
+else:
+    res = list(map(lambda x: x[1], model.predict_proba(
+        test_data.values)))
+    loss = log_loss(test_labels, res)
 
 
-print("{} correct predictions in {} total: {}%".format(
-    correct, len(predictions), correct/len(predictions)*100))
-
-print("There were {} real canceled flights".format(canceled))
+predictions = zip(res, test_labels)
