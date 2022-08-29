a5c0fa2baa6dcb277b346a9798b0ac33bd8bf85c
Manuele Lucchi <m.lucchi@egeatech.com>
2022-08-29 20:18:26 +0200

    improvements in tests

--- src/main.py
+++ src/main.py
@@ -11,21 +11,42 @@ train_data, train_labels, test_data, test_labels = preprocess()
 print('Train Data {}, Train Labels {}, Test Data {}, Test Labels {}'.format(
     train_data.shape, train_labels.shape, test_data.shape, len(test_labels)))
 
-if sys.argv[2] == "custom":
-    model = Model(batch_size=train_data.shape[0], learning_rate=1)
+n_features = train_data.shape[0]
+
+
+def custom_train_eval(lr=1, iterations=100) -> float:
+    model = Model(batch_size=train_data.shape[1], learning_rate=lr)
     model.train(train_data.to_numpy(),
-                train_labels.to_numpy(), iterations=100)
-else:
+                train_labels.to_numpy(), iterations=iterations)
+    res = model.forward(test_data.values)
+    return binary_cross_entropy(res, test_labels.values)
+
+
+def sklearn_train_eval() -> float:
     model = LogisticRegression()
     model.fit(train_data.to_numpy(), train_labels.to_numpy())
-
-if sys.argv[2] == "custom":
-    res = model.forward(test_data.values)
-    loss = binary_cross_entropy(res, test_labels.values)
-else:
     res = list(map(lambda x: x[1], model.predict_proba(
         test_data.values)))
-    loss = log_loss(test_labels.values, res)
+    return log_loss(test_labels.values, res)
 
 
-predictions = zip(res, test_labels.values)
+def experiment(n, func):
+    res: float = func()
+    print("Experiment {} completed with result: {}".format(n, res))
+
+
+lr1 = 0.01
+lr2 = 0.001
+lr3 = 0.0001
+lr4 = 0.00001
+it1 = 100
+it2 = 500
+it3 = 1000
+
+experiment(1, lambda: custom_train_eval(lr=lr1, iterations=it1))
+experiment(2, lambda: custom_train_eval(lr=lr2, iterations=it1))
+experiment(3, lambda: custom_train_eval(lr=lr3, iterations=it1))
+experiment(4, lambda: custom_train_eval(lr=lr4, iterations=it1))
+experiment(5, lambda: custom_train_eval(lr=lr3, iterations=it2))
+experiment(6, lambda: custom_train_eval(lr=lr3, iterations=it3))
+experiment("Sklearn", lambda: sklearn_train_eval())
